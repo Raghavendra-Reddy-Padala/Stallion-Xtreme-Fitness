b25dd9ab7e118b76243a2b29b89db8d746d7b24f
Vishal Chennuri <155214168+vishalchennuri@users.noreply.github.com>
2025-08-08 13:25:46 +0530

    Update page.tsx

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from "@/components/events/HeroSection";
 import Highlights from "@/components/events/Highlights";
-import EventsSection from "@/components/events/UpcomingEvents";
 import WhyJoinUs from "@/components/events/WhyJoinUs";
 import Footer from "@/components/layout/Footer";
 import ContactSection from "@/components/sections/ContactSection";
